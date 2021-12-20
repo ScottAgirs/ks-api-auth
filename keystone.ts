f1426526472dd07e81551d63b967e6dd27ff8566
ScottAgirs <scott@ijs.to>
2021-12-20 11:50:25 +0200

    feat(DB): Implement PG
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- keystone.ts
+++ keystone.ts
@@ -26,11 +26,18 @@ export default withAuth(
         credentials: true,
       },
     },
-    // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
+      provider: 'postgresql',
+      url: process.env.DATABASE_URL as string,
+      async onConnect(keystone) {
+        console.log("CONNECTED TO DB");
+      },
     },
+    // the db sets the database provider - we're using sqlite for the fastest startup experience
+    // db: {
+    //   provider: 'sqlite',
+    //   url: 'file:./keystone.db',
+    // },
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
       // For our starter, we check that someone has session data before letting them see the Admin UI.
